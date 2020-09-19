3e09c619e8885920417693b41fa0e9143a7b5337
Chris Nosowsky <ChrisNosowsky@gmail.com>
2020-09-18 23:52:51 -0400

    initial project setup

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace university_enrollment.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
