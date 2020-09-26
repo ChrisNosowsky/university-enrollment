92d0ab57ea598c022cfb478e81b2a7a934329190
Chris Nosowsky <ChrisNosowsky@gmail.com>
2020-09-26 16:25:00 -0400

    yeet

--- DAL/SchoolContext.cs
+++ DAL/SchoolContext.cs
@@ -10,14 +10,16 @@ namespace university_enrollment.DAL
 {
     public class SchoolContext : DbContext
     {
-        public SchoolContext() : base("SchoolContext") { }
+        public SchoolContext() : base("SchoolContext") { } //schoolcontext is conn. string
 
         public DbSet<Student> Students { get; set; }
         public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            // don't plural tables
         }
     }
 }
