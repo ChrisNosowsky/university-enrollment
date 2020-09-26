2daf9b1d33cbbb13cd2406af4fa119025b298014
Chris Nosowsky <ChrisNosowsky@gmail.com>
2020-09-26 16:20:15 -0400

    db context and data models added

---
+++ DAL/SchoolContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Threading.Tasks;
+using university_enrollment.Models;
+
+namespace university_enrollment.DAL
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base("SchoolContext") { }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace university_enrollment.Models
+{
+    public class Student
+    {
+    }
+}
